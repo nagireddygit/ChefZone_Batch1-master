ea0e7b43a1778b864b51dbb8cec7bf9126344066
Palnadu <reddy.scm44@gmail.com>
2016-11-14 20:59:57 +0530

    chefZone_batch1

---
+++ README.md
@@ -0,0 +1,1 @@
+# ChefZone_Batch1
---
+++ demo/cookbooks/webapp-linux/recipes/apacheold.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: .
+# Recipe:: apacheold
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+package node["webapp-linux"]["packagename"]  do 
+  action :install
+  only_if { node["platform"] == node["webapp-linux"]["platformname"]}
+end
+
